f390440e6cb814f22e8e19ca9b2e4a7ef658f898
Étienne <1372556@cegepmontpetit.ca>
2023-09-25 17:40:21 -0400

    FCT Roster HTML et CSS
    Ajout du code pour roster.component.html et roster.component.css

--- TP1/src/app/roster/roster.component.ts
+++ TP1/src/app/roster/roster.component.ts
@@ -1,5 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+interface Joueur {
+  jerseyNum: string;
+  pos: string;
+  espnName: string;
+  school: string;
+  age: string;
+    height: string;
+    weight: string;
+  stats: {
+    gamesPlayed: string;
+    teamID: string;
+    espnHeadshot: string;
+  };
+  // Ajoutez d'autres propriétés ici en fonction de la structure de vos données
+}
 
 @Component({
   selector: 'app-roster',
@@ -8,17 +25,34 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class RosterComponent implements OnInit {
   equipeSelectionnee: string | undefined;
+  joueurs: { body: { roster: Joueur[] } } = { body: { roster: [] } };
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private http: HttpClient) { }
 
   ngOnInit(): void {
-    // Récupérez le paramètre de l'équipe sélectionnée depuis l'URL
     this.route.params.subscribe(params => {
       this.equipeSelectionnee = params['equipe'];
-      // Utilisez cette valeur pour afficher le roster de l'équipe sélectionnée
       console.log('Équipe sélectionnée :', this.equipeSelectionnee);
-      // Chargez ici les données du roster de l'équipe sélectionnée si nécessaire
+      this.chargerRoster();
     });
   }
-}
 
+  chargerRoster() {
+    const apiUrl = `https://tank01-nfl-live-in-game-real-time-statistics-nfl.p.rapidapi.com/getNFLTeamRoster?teamAbv=${this.equipeSelectionnee}&getStats=true`;
+
+    const headers = new HttpHeaders({
+      'X-RapidAPI-Key': 'c9e94bd108msh320bd6bbae9f021p1a3603jsn0473b241db85',
+      'X-RapidAPI-Host': 'tank01-nfl-live-in-game-real-time-statistics-nfl.p.rapidapi.com',
+    });
+
+    this.http.get<any>(apiUrl, { headers }).subscribe(
+      (data) => {
+        this.joueurs = data;
+        console.log(this.joueurs);
+      },
+      (error) => {
+        console.error('Erreur lors de la récupération du roster :', error);
+      }
+    );
+  }
+}
